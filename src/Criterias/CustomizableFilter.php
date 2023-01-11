1b60036b61f4281554d3214eb0f61197e27f37e5
Nils Refle <n.refle@proengeno.de>
2023-01-11 18:18:53 +0100

    Add CustomizableFilter

---
+++ src/Criterias/CustomizableFilter.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Apfelfrisch\QueryFilter\Criterias;
+
+use Apfelfrisch\QueryFilter\Conditions\OrWhereCondition;
+use Apfelfrisch\QueryFilter\Conditions\SortCondition;
+use Apfelfrisch\QueryFilter\Conditions\WhereCondition;
+use Apfelfrisch\QueryFilter\QueryBuilder;
+
+final class CustomizableFilter implements Criteria
+{
+    private array $conditions;
+
+    public function __construct(
+        private string $name,
+        WhereCondition|OrWhereCondition|SortCondition ...$conditions
+    ) {
+        $this->conditions = $conditions;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getType(): Type
+    {
+        return Type::Filter;
+    }
+
+    public function apply(QueryBuilder $builder): QueryBuilder
+    {
+        $builder->where(
+            ...array_filter($this->conditions, function(WhereCondition|OrWhereCondition|SortCondition $condition) {
+                return ! $condition instanceof SortCondition;
+            })
+        );
+
+        $sorts = array_filter($this->conditions, function(WhereCondition|OrWhereCondition|SortCondition $condition) {
+            return $condition instanceof SortCondition;
+        });
+
+        foreach ($sorts as $sort) {
+            $builder->sort($sort->name, $sort->sortDirection);
+        }
+
+        return $builder;
+    }
+}
